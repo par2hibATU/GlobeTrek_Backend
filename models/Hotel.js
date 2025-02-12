148b53ab054ff2f1bba2eea7304f97dc39a06113
par2hibATU <parthibbdcox@gmail.com>
2025-02-12 06:51:42 +0000

    nothing

--- models/Hotel.js
+++ models/Hotel.js
@@ -25,6 +25,10 @@ const HotelSchema = new mongoose.Schema({
         type: String,
         required:true
     },
+    title:{
+        type: String,
+        required:true
+    },
     desc:{
         type: String,
         required:true
