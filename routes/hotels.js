474270008db2da7f5e5189aac539fbb4ea5bba4b
par2hibATU <parthibbdcox@gmail.com>
2025-02-14 09:28:52 +0000

    Specific messages with the errors are added

--- routes/hotels.js
+++ routes/hotels.js
@@ -46,12 +46,12 @@ router.get("/:id", async (req,res)=>{
 
 
 //get all
-router.get("/", async (req,res)=>{
+router.get("/", async (req, res, next)=>{
     try{
         const hotels = await Hotel.find();
         res.status(200).json(hotels)
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
 })
 export default router
