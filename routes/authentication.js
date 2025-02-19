0a0478e0c04926d8f6a050042627005ef9c72994
par2hibATU <parthibbdcox@gmail.com>
2025-02-19 01:09:21 +0000

    JSON template for new user registration is added

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,12 +2,7 @@ import express from "express"
 
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.send("Hello, this authentication endpoints")
-})
 
-router.get("/reg", (req, res)=>{
-    res.send("Hello, this register endpoints")
-})
+router.get("/register", )
 
 export default router
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,60 +1,26 @@
 import express from "express"
 import Hotel from "../models/Hotel.js";
 import createError from "http-errors";
+import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotel.js";
+ 
 
 
 const router = express.Router();
 
 // create 
-router.post("/", async (req,res)=>{
+router.post("/", createHotel);
 
-    const newHotel = new Hotel(req.body)
 
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
 //update
-router.put("/:id", async (req,res)=>{
-    try{
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body}, { new: true })
-        res.status(200).json(updatedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.put("/:id", updateHotel)
 
 //delete
-router.delete("/:id", async (req,res)=>{
-    try{
-        await Hotel.findByIdAndDelete(req.params.id)
-        res.status(200).json("This Hotel has been deleted")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.delete("/:id", deleteHotel)
+
 //get
-router.get("/:id", async (req,res)=>{
-    try{
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id", getHotel)
 
 
 //get all
-router.get("/", async (req, res, next)=>{
-
-    try{
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels)
-    }catch(err){
-        next(err)
-    }
-})
+router.get("/", getAllHotels)
 export default router
