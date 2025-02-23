1f4c9ebe60b2e527ac0987337a04c610605753af
par2hibATU <parthibbdcox@gmail.com>
2025-02-23 21:52:04 +0000

    fixed a bug

--- utills/verifyToken.js
+++ utills/verifyToken.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
-import { createError } from "../utills/error.js"
+import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) =>{
     const token = req.cookies.access_token;
     if(!token){
         return next(createError(401, "You are not authenticated!"))
     }
-    jwt.verify(token, process.env.JWT, (err, use)=>{
+    jwt.verify(token, process.env.JWT, (err, user)=>{
         if(err) return next(createError(403, "Token is not Valid!"));
         req.user = user;
         next();
@@ -15,11 +15,13 @@ export const verifyToken = (req, res, next) =>{
 };
 
 export const verifyUser = (req, res, next)=>{
-    verifyToken(req, res, ()=>{
+    verifyToken(req, res, (err)=>{
+        if (err) return next(err);
+
         if(req.user.id === req.params.id || req.user.isAdmin){
             next()
         }else{
-            if(err) return next(createError(403, "You are not authorized!"));
+            return next(createError(403, "You are not authorized!"));
 
         }
     })
