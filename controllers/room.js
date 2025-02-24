b35b1fae64e90102a11c6b76fd625a1f7d9a6e5a
par2hibATU <parthibbdcox@gmail.com>
2025-02-24 20:14:08 +0000

    Fixed room bug associated with hotels

--- controllers/room.js
+++ controllers/room.js
@@ -3,24 +3,30 @@ import Hotel from "../models/Hotel.js";
 import { createError } from "../utills/error.js";
 
 
-export const createRoom = async (req, res, next) =>{
-    const hotelId = req.params.hotelid;
-    const newRoom = new Room(req.body)
+export const createRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelId;  // Ensure this matches the URL param correctly
+    const newRoom = new Room(req.body);
 
-    try{
-        const savedRoom = await newRoom.save()
-        try{
-            await Hotel.findByIdAndUpdate(hotelId, {$push : { rooms: savedRoom._id },
-            });
-        }catch(err){
-            next(err)
+    try {
+        const savedRoom = await newRoom.save();
+        
+        // Ensure the Hotel document gets updated properly
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            hotelId,
+            { $push: { rooms: savedRoom._id } },  
+            { new: true, useFindAndModify: false }  
+        );
+
+        if (!updatedHotel) {
+            return next(createError(404, "Hotel not found"));
         }
-        res.status(200).json(savedRoom);
-    }catch(err){
-        next(err)
-    }
 
-}
+        res.status(200).json(savedRoom);
+    } catch (err) {
+        next(err);
+    }
+};
+
 
 export const updateRoom = async(req, res, next)=>{
     try{
